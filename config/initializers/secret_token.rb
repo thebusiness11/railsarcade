a623aa289fb23f0b79092e5d5587433640b660aa
Lance Velasco <lance@mlvgroup.com>
2012-12-29 12:03:03 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsarcade::Application.config.secret_token = 'ffe2c978b66475ff29514f17d4e63bbffa43a78380407c2d649b31dc8a48d6d85f507e7d16d83bf3425bcfd875a1a63a45e30c7a488c79f746f71ecd53889a8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Railsarcade::Application.config.session_store :cookie_store, key: '_railsarcade_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Railsarcade::Application.config.session_store :active_record_store
